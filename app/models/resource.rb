f8c7afaa8fa892211a590053e867c9b44670d367
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2020-02-15 10:42:47 +0400

    Create Resource model with base64 attached image. Add Rubocop

---
+++ app/models/resource.rb
@@ -0,0 +1,8 @@
+class Resource < ApplicationRecord
+  has_one_base64_attached :image
+
+  validates :name, presence: true
+
+  validates :image, attached: true
+  validates :image, data_base64_uri: true
+end
---
+++ app/validators/data_base64_uri_validator.rb
@@ -0,0 +1,9 @@
+class Database64UriValidator < ActiveModel::EachValidator
+  DATA_BASE64_URI_REGEXP = %r{/^data:.+;base64,.+$/}.freeze
+
+  def validate_each(record, attribute, value)
+    return unless DATA_BASE64_URI_REGEXP.match?(value)
+
+    record.errors.add(attribute, :data_base64_uri, options)
+  end
+end
