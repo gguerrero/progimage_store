5a110b12d65bc2a348d27dad55e812fc8b0b5d2a
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2020-02-16 10:15:02 +0400

    Add test for ResourceIdSerializer

---
+++ spec/serializers/api/v1/resource_id_serializer_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ResourceIdSerializer do
+  let(:ruby_png) { File.open('spec/fixtures/ruby.png') }
+  let(:base64_data_uri) {
+    generate_data64_uri file: ruby_png, content_type: 'image/png'
+  }
+  let(:resource) do
+    resource = Resource.new(name: 'Ruby', description: 'Is ruby logo!')
+    resource.attach_image(base64_data_uri)
+    resource.save!
+
+    resource
+  end
+
+  context 'serializing it as JSON' do
+    it 'returns a hash with the expected attributes' do
+      serializer = Api::V1::ResourceIdSerializer.new(resource)
+      data = serializer.serializable_hash[:data]
+
+      expect(data).to eq(id: resource.id, type: :resource_id)
+    end
+  end
+end
